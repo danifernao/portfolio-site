65f75ff9ddb0d93436e58688be7e79bd72b73845
Daniel <82724282+dotdani@users.noreply.github.com>
2025-06-29 09:54:31 -0500

    refactor: limpia y reorganiza componentes y tipos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { HeaderType, LinksItemType } from "../types/types";
-import HeaderLinks from "./HeaderLinks";
 import { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface HeaderProps {
   dataHeader: HeaderType;
@@ -19,6 +20,7 @@ function Header({ dataHeader, dataLinks }: HeaderProps) {
 
   return (
     <div id="header">
+      {/* Columna del avatar */}
       {dataHeader.photo && (
         <div>
           <a
@@ -43,15 +45,37 @@ function Header({ dataHeader, dataLinks }: HeaderProps) {
           />
         </div>
       )}
-      <div>
+
+      {/* Columna de la información general */}
+      <div className="info-col">
         {dataHeader.name && (
           <h1>
             <a href="/">{dataHeader.name}</a>
           </h1>
         )}
+
         {dataHeader.profession && <p>{dataHeader.profession}</p>}
         {dataHeader.address && <p>{dataHeader.address}</p>}
-        {dataLinks.length > 0 && <HeaderLinks links={dataLinks} />}
+
+        {dataLinks.length > 0 && (
+          <ul>
+            {dataLinks.map((link, i) => (
+              <li key={i}>
+                <a
+                  href={link.url}
+                  title={link.title}
+                  aria-label={link.title}
+                  target="_blank"
+                >
+                  <FontAwesomeIcon
+                    icon={["fab", link.icon] as IconProp}
+                    aria-hidden={true}
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
